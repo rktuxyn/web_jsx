c79ee15e8182a37cfab3c81236ae09178e3cf8ae
RKTUXYN <mssclang@outlook.com>
2019-01-23 19:48:34 +0600

    Change <js==js> parser

--- web_jsx/src/script_tag_parser.cpp
+++ web_jsx/src/script_tag_parser.cpp
@@ -1,7 +1,8 @@
 #include "script_tag_parser.h"
 using namespace sow_web_jsx;
 jsx_export void sow_web_jsx::js_write_header(std::stringstream& stream) {
-	stream << "context.response.write = function write( val ) { if ( !val || val === null || val === undefined ) return;  /*if( typeof( val )===\"string\" ) val = val.trim();*/ context.response._write( val + '\\r\\n' );};\r\n";
+	stream << "context.response.write = function write( val, nline ) { if ( !val || val === null || val === undefined ) return this;  !1 === nline ? context.response._write( val ) : context.response._write( val + '\\r\\n' ); return this;};\r\n";
+	stream << "context.response.write_p = function write_p( val ) { if ( !val || val === null || val === undefined ) return this; context.response._write( val ); return this;};\r\n";
 	stream << "context.https = context.https===\"on\" || context.https===\"true\" ? true : false;\n";
 	stream << "context.protocol= context.https===true ? \"https:\" : \"http:\";\n";
 	stream << "context.host_url = context.protocol + \"//\" + context.host;\n";
@@ -11,23 +12,14 @@ jsx_export void sow_web_jsx::js_write_header(std::stringstream& stream) {
 	stream << "context.response.header(\"Connection\", \"Keep-Alive\");\n";
 	stream << "context.root_dir = (function () { if ( context.root_dir === undefined || context.root_dir === null ) { throw \"Root directory not found in current context!!!\"; } return context.root_dir.replace( /\\//g,'\\\\' ) }());\n";
 	stream << "context.request.query_string = ( function () { try { return JSON.parse( (context.request.query_string) ); } catch ( e ) { return {}; } }() );\n";
+	stream << "String.format = function ( format ) {if ( typeof ( format ) !== 'string' ) throw new Error(\"String required!!!\"); let args = Array.prototype.slice.call( arguments, 1 ); let len = args.length - 1; return format.replace( /{(\\d+)}/g, function ( match, number )  { let index = parseInt( number ); if ( isNaN( index ) ) throw new Error( \"Invalid param index!!!\" ); if ( index > len ) throw new Error( \"Index should not greater than \" + len + format + JSON.stringify( args ) ); return typeof ( args[index] ) !== 'undefined' ? args[number] : \"\"; } ); };\n";
 	stream << "try{\n";
 };
 jsx_export void sow_web_jsx::js_write_footer(std::string&str) {
 	str.append("context.response.header(\"X-Powered-By\", \"safeonline.world\");\n");
 	str.append("context.response.header(\"X-Process-By\", \"web_jsx_cgi\");\n");
 	str.append("context.response.body.flush();\n");
-	str.append("}catch(_exp){\n\
-				context.response.clear();\n\
-				context.response.header(\"Content-Type\", \"text/html\");\n\
-				if(typeof(_exp)!==\"object\"){\n\
-					context.response.write('Error::' + _exp );\n\
-				}else{\n\
-					context.response.write('Error::' + _exp.message + '<br/>' );\n\
-					context.response.write(_exp.stack );\n\
-				}\n\
-				context.response.body.flush();\n\
-			};");
+	str.append("}catch(_exp){\n\context.response.clear();\n\context.response.header(\"Content-Type\", \"text/html\");\n\if(typeof(_exp)!==\"object\"){\n\context.response.write('Error::' + _exp );\n\}else{\n\context.response.write('Error::' + _exp.message + '<br/>' );\n\context.response.write(_exp.stack );\n\}\n\context.response.body.flush();\n\};");
 };
 jsx_export void sow_web_jsx::js_write_footer(std::stringstream& stream) {
 	stream << "context.response.header(\"X-Powered-By\", \"safeonline.world\");\n";
@@ -48,6 +40,7 @@ jsx_export void sow_web_jsx::js_write_footer(std::stringstream& stream) {
 jsx_export void sow_web_jsx::js_write_console_header(std::stringstream& stream) {
 	stream << "__root_dir = ( function () { if ( __root_dir === undefined || __root_dir === null ) { throw new Error(\"Root directory not found in current context!!!\"); } return __root_dir.replace( /\\//g,'\\\\' ) }());\n";
 	stream << "__arg = ( function () { try { return JSON.parse( __arg ); } catch ( e ) { __print( e.message ); return []; } }() );\n";
+	stream << "String.format = function ( format ) {if ( typeof ( format ) !== 'string' ) throw new Error(\"String required!!!\"); let args = Array.prototype.slice.call( arguments, 1 ); let len = args.length - 1; return format.replace( /{(\\d+)}/g, function ( match, number )  { let index = parseInt( number ); if ( isNaN( index ) ) throw new Error( \"Invalid param index!!!\" ); if ( index > len ) throw new Error( \"Index should not greater than \" + len + format + JSON.stringify( args ) ); return typeof ( args[index] ) !== 'undefined' ? args[number] : \"\"; } ); };\n";
 	stream << "try{\n";
 };
 jsx_export void sow_web_jsx::js_write_console_footer(std::stringstream& stream) {
@@ -82,13 +75,12 @@ class js_parser {
 public:
 	js_parser() {
 		script_tag *tags = new script_tag();
-		this->regx_sts = new std::regex("\\" + tags->sts);
-		this->regx_ste = new std::regex("\\" + tags->ste);
-		this->regx_rts = new std::regex("\\" + tags->rts);
-		this->regx_rte = new std::regex("\\" + tags->rte);
+		this->regx_sts = new std::regex("(" + tags->sts + ")");
+		this->regx_ste = new std::regex("(" + tags->ste + ")");
+		this->regx_rts = new std::regex("(" + tags->rts + ")");
+		this->regx_rte = new std::regex("(" + tags->rte + ")");
 		this->rep_str = "88_REP__STR__88";
-		this->rep_str_regex = new std::regex(this->rep_str);
-		this->result_key = "_nopr_";
+		this->rep_str_regex = new std::regex("(" + this->rep_str + ")");
 		delete tags;
 	}
 	~js_parser() {
@@ -98,7 +90,6 @@ public:
 		delete this->regx_rte;
 		delete this->rep_str_regex;
 		std::string().swap(this->rep_str);
-		std::string().swap(this->result_key);
 	};
 private:
 public:
@@ -108,7 +99,6 @@ public:
 	std::regex* regx_rte;
 	std::string rep_str;
 	std::regex* rep_str_regex;
-	std::string result_key;
 	int start_tag(std::regex&regx, parser_info&info) {
 		if (REGEX_IS_MATCH(info.line, regx) == 0) {
 			if (info.is_tag_end && info.etag != e_tag::sts) {
@@ -123,36 +113,32 @@ public:
 			if (REGEX_IS_MATCH(info.line, *this->regx_ste) == 0) {
 				info.is_tag_end = false;
 				info.line = std::regex_replace(info.line, regx, " " + this->rep_str + ");");
-				//info.line.append("\r\n");
-				//info.line = std::regex_replace(info.line, std::regex("'"), " " + this->rep_str);
 				break;
 			}
 			info.is_tag_end = true;
 			info.is_tag_start = false;
 			info.line = std::regex_replace(info.line, regx, " " + this->rep_str + ");");
-			//info.line = std::regex_replace(info.line, *this->regx_ste, "");
 			info.line = std::regex_replace(info.line, *this->regx_ste, "\r\ncontext.response.write(" + this->rep_str);
 			break;
 		case e_tag::rts/*<js=*/:
 			if (REGEX_IS_MATCH(info.line, *this->regx_rte) == 0) {
 				info.is_tag_end = false;
-				//info.line = std::regex_replace(info.line, std::regex("'"), " " + this->rep_str);
 				info.line = std::regex_replace(info.line, regx, "");
 				info.line.append(this->rep_str + ");\r\ncontext.response.write(");
 				break;
 			}
 			info.is_tag_end = true;
 			info.is_tag_start = false;
-			info.line = REGEX_REPLACE_MATCH(info.line, std::regex("/<js=(.+?)=js>/g"),
-				[=](const std::smatch& m) {
+			info.line = REGEX_REPLACE_MATCH(info.line, std::regex("(<js=(.+?)=js>)"),
+				[&](const std::smatch& m) {
 				std::string matchstr(m.str(0));
 				if (matchstr.empty()) {
 					return std::string("");
 				}
-				return matchstr;// std::regex_replace(matchstr, std::regex("'"), " " + this->rep_str);
+				matchstr = std::regex_replace(matchstr, *this->regx_rts, this->rep_str + ", !1).write_p(");
+				matchstr = std::regex_replace(matchstr, *this->regx_rte, ").write(" + this->rep_str);
+				return matchstr;
 			});
-			info.line = std::regex_replace(info.line, regx, " " + this->rep_str + "); context.response.write(");
-			info.line = std::regex_replace(info.line, *this->regx_rte, ");context.response.write(" + this->rep_str);
 			break;
 		default: _default = false; break;
 		}
@@ -190,10 +176,9 @@ public:
 		return 0;
 	};
 };
-int script_tag_parser::parse(template_result & tr,
-	std::string&html_body) {
-	std::regex*regx_sts = new std::regex("\\<js::");
-	std::regex*regx_str = new std::regex("\\<js=");
+int script_tag_parser::parse(template_result & tr, std::string&html_body) {
+	std::regex*regx_sts = new std::regex("(<js::)");
+	std::regex*regx_str = new std::regex("(<js=)");
 	if (REGEX_IS_MATCH(html_body, *regx_sts) == 0) {
 		delete regx_sts;
 		if (REGEX_IS_MATCH(html_body, *regx_str) == 0) {
@@ -216,21 +201,16 @@ int script_tag_parser::parse(template_result & tr,
 	std::regex* rgx = new std::regex("(?:\\r\\n|\\r|\\n)");
 	std::sregex_token_iterator iter(html_body.begin(), html_body.end(), *rgx, -1);
 	std::regex* qu = new std::regex("'");
-	//std::regex* ws = new std::regex("/^\\s*|\\s*$");
-	//std::regex*tab = new std::regex("(?:\\t)");
 	parser_info* info = new parser_info();
-	js_parser* jsp = new js_parser();//(js_parser*)malloc(sizeof js_parser);
+	js_parser* jsp = new js_parser();
 	info->is_tag_start = false;
 	info->is_tag_end = true;
 	for (std::sregex_token_iterator end; iter != end; ++iter) {
 		info->tag_found = false;
 		info->line = iter->str();
-		//info->line = std::regex_replace(info->line, *tab, "");
 		if (info->line.empty()) {
-			//stream << jsp->result_key << " += '';";
 			continue;
 		}
-		//i++;
 		if (info->is_tag_end) {
 			info->line = std::string("context.response.write(" + jsp->rep_str + info->line);
 			stream << "\n";
@@ -260,18 +240,8 @@ int script_tag_parser::parse(template_result & tr,
 	delete qu; delete jsp;
 	html_body = stream.str();
 	std::stringstream().swap(stream);
-	//stream << "\n/** ** [/END STRIPE-" << std::to_string(i + 1 - 1) << "] ** **/\n/** ** [END CLIENT SCRIPT]** **/\n";
-	//html_body = std::regex_replace(html_body, *ws, " ");
-	//delete ws;
-	//html_body = std::regex_replace(html_body, std::regex("(?:" + jsp->result_key + " \\+\\= '';)"), "");
-	//delete jsp;
 	html_body = std::regex_replace(html_body, std::regex("(?:context.response.write\\(''\\);)"), "");
 	html_body = std::regex_replace(html_body, std::regex("(?:context.response.write\\(' '\\);)"), "");
-	//html_body = std::regex_replace(html_body, std::regex("(?:context.response.write\\('  '\\);)"), "");
-	if (tr.remove_new_line) {
-		//html_body = std::regex_replace (html_body, std::regex("\\n\\s*\\n"), "\r\n");
-		//html_body = std::regex_replace (html_body, std::regex("\\r\\n\\s*\\r\\n"), "\n");
-	}
 	js_write_footer(html_body);
 	return 1;
 };
